f9a8714bb32daa87cca3266f6722e335d8911a26
Alessandro de Oliveira <alessandro.wolfera@gmail.com>
2020-06-10 11:23:49 -0300

    Resolução de exercicio sem correção

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AluguelDeQuartos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many rooms will be rent? ");
+            int quant = int.Parse(Console.ReadLine());
+
+            Quarto[] vect = new Quarto[9];
+
+            for ( int i = 0; i < quant; i++)
+            {
+                string nome, email;
+                int numero;
+                Console.WriteLine();
+                Console.WriteLine("RENT #"+ (i + 1) + ": ");
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+
+                Console.Write("Numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                vect[i] = new Quarto { Inquilino = nome, Email = email, Numero = numero };
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("BUSY ROOMS");
+            Console.WriteLine();
+            for (int i = 0; i < quant; i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(vect[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Quarto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AluguelDeQuartos
+{
+    class Quarto
+    {
+        public int Numero { get; set; }
+        public string Inquilino { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return (Numero +
+                ": " + 
+                Inquilino
+                + ", " + 
+                Email);
+        }
+    }
+}
